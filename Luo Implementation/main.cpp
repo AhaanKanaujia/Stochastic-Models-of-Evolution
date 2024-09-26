8da26044f1e3fc0b24e62e7b9f0282e8ece1819f
AhaanKanaujia <kanaujia.ahaan@gmail.com>
2024-09-26 18:07:57 -0500

    fixed luo implementation and optimized prob calc

--- Luo Implementation/main.cpp
+++ Luo Implementation/main.cpp
@@ -8,19 +8,20 @@ using namespace std;
 vector<double> get_leaving_rates(vector<double> u, int m, int n, double r, double s) {
     vector<double> L(n + 1, 0.0);
 
+    double sum = 0.0;
     for(int i = 0; i < n + 1; i++) {
-        double sum = 0.0;
-        for (int j = 0; j < n + 1; j++) {
-            if (j != i) {
-                sum += m * u[j] * (1 + r * j/n);
-            }
-        }
+        sum += u[i] * (1 + r * i/n);
+    }
+
+    for(int i = 0; i < n + 1; i++) {
+        double term_sum = sum - u[i] * (1 + r * i/n);
+
         if (i == 0 || i == n) {
             // m * u[i] to the term to ensure positive probabilities
             // fixed typo from the paper
-            L[i] = m * u[i] * sum;
+            L[i] = m * u[i] * term_sum;
         } else {
-            L[i] = m * u[i] * (n - i) * i/n * (2 + s) + m * u[i] * sum;
+            L[i] = m * u[i] * (n - i) * i/n * (2 + s) + m * u[i] * term_sum;
         }
     }
 
@@ -68,11 +69,11 @@ int draw_random_number(vector<double> draw_prob, string type) {
         prob[i] = draw_prob[i] / sum;
     }
 
-    cout << type;
-    for(int t = 0; t < prob.size(); t++) {
-        cout << prob[t] << " ";
-    }
-    cout << "\n";
+    // cout << type;
+    // for(int t = 0; t < prob.size(); t++) {
+    //     cout << prob[t] << " ";
+    // }
+    // cout << "\n";
 
     discrete_distribution<int> dd(prob.begin(), prob.end());
 
@@ -98,8 +99,8 @@ vector<double> randomize_initial_distribution(int m, int n) {
 }
 
 int main() {
-    int m = 10; // number of groups
-    int n = 3; // number of individuals in a group
+    int m = 50; // number of groups
+    int n = 50; // number of individuals in a group
 
     double r = 0.1; // proportionality constant
     double s = 0.05; // proportionality constant
@@ -116,11 +117,11 @@ int main() {
     double T = 0.0; // time
 
     while(fabs(u[0] - 1.0) > 1e-8 && fabs(u[n] - 1.0) > 1e-8) {
-        cout << "U: ";
-        for (int i = 0; i < u.size(); i++) {
-            cout << u[i] << " ";
-        }
-        cout << "\n";
+        // cout << "U: ";
+        // for (int i = 0; i < u.size(); i++) {
+            // cout << u[i] << " ";
+        // }
+        // cout << "\n";
 
         // rate of balls leaving each group
         vector<double> L = get_leaving_rates(u, m, n, r, s);
@@ -131,12 +132,12 @@ int main() {
 
         // draw random number I1, group that the individual is drawn from
         int I1 = draw_random_number(L, "(L) = Drawing Group Prob: ");
-        cout << "Drawing Group Index: " << I1 << endl;
+        // cout << "Drawing Group Index: " << I1 << endl;
 
         // draw random number I2, group that the individual is placed in
         vector<double> G = get_incoming_rates(u, m, n, r, s, I1);
         int I2 = draw_random_number(G, "(G) = Placed Group Prob: ");
-        cout << "Placed Group Index: " << I2 << endl;
+        // cout << "Placed Group Index: " << I2 << endl;
 
         // update u and t
         u[I1] -= 1.0/m;
@@ -144,9 +145,9 @@ int main() {
         T += tau;
 
         // cout << "Time Taken for Current Event: " << tau << endl;
-        cout << "Total Time: " << T << endl;
+        // cout << "Total Time: " << T << endl;
 
-        cout << "\n";
+        // cout << "\n";
     }
 
     cout << "Final U: ";
@@ -154,6 +155,8 @@ int main() {
         cout << u[i] << " ";
     }
     cout << "\n";
+
+    cout << "Total Time Taken: " << T << endl;
 
     return 1;
 }
